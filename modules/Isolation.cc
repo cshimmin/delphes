defb6084e1200a1e1d80d897ddad94b638b7cf64
Chase Shimmin <cshimmin@uci.edu>
2015-05-22 08:58:16 -0700

    Revert overlap bug in isoloation module

--- modules/Isolation.cc
+++ modules/Isolation.cc
@@ -187,7 +187,8 @@ void Isolation::Process()
       const TLorentzVector &isolationMomentum = isolation->Momentum;
 
       if(candidateMomentum.DeltaR(isolationMomentum) <= fDeltaRMax &&
-         candidate->GetUniqueID() != isolation->GetUniqueID())
+           !candidate->Overlaps(isolation))
+//         candidate->GetUniqueID() != isolation->GetUniqueID())
       {
         sum += isolationMomentum.Pt();
         ++counter;
